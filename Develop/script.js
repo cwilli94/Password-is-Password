a09fc90cabb70ef9c3e84a84f8de02a41f1eabad
conner <conner.williams4@gmail.com>
2022-12-11 18:05:49 -0500

    added variables and start of generatepassword function

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,15 @@
 // Assignment code here
+var passwordlength = prompt("How many characters should the password be? Select 8-128 charcters.")
+var passworduppercase = confirm("Click OK to include uppercase character.")
+var passwordlowercase = confirm("click OK to include lowercase letters")
+var passwordnumber = confirm("Click OK to include numbers.")
+var passwordspecial = confirm("Click OK to include special characters.")
 
 
+function generatePassword(){
+  
+
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
